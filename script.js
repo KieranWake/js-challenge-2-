25bf74cababd509d66c5f61a256f570a4385fd90
Kieran Wake <kieranwake1234@gmail.com>
2024-01-27 11:02:52 +0000

    implemented two const variables with math min and math max applied. test failed

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function (world) {
+// Need to create a function that takes an array of numbers and returns both the minimum and - 
+// maximum numbers in that order. 
+
+// examples: 
+//minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+//minMax([2334454, 5]) ➞ [5, 2334454]
+//minMax([1]) ➞ [1, 1]
+
+//All test arrays will have at least one element and are valid.
+
+function minMax(arr) {
     
+	
 }
